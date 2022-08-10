2c215d89abf89e28959dd00b8902f4738e64e331
BartoszJS <bartosz290300@wp.pl>
2022-08-10 12:58:10 +0200

    7 before webpack

--- migrations/Version20220808212627.php
+++ migrations/Version20220808212627.php
@@ -22,7 +22,7 @@ final class Version20220808212627 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE actor (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE movie (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, release_year INT NOT NULL, description VARCHAR(255) DEFAULT NULL, image_path VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE movie_actor (movie_id INT NOT NULL, actor_id INT NOT NULL, INDEX IDX_3A374C658F93B6FC (movie_id), INDEX IDX_3A374C6510DAF24A (actor_id), PRIMARY KEY (movie_id, actor_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE ENGINE= InnoDB');
+        $this->addSql('CREATE TABLE movie_actor (movie_id INT NOT NULL, actor_id INT NOT NULL, INDEX IDX_3A374C658F93B6FC (movie_id), INDEX IDX_3A374C6510DAF24A (actor_id), PRIMARY KEY (movie_id, actor_id))');
 
         
     }
--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,20 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'app_movies' )]
     public function index(): Response
     {
-        $movies = ["Avengers","Inception","Loki"];
-        return $this->render('index.html.twig' , array(
-            'movies' => $movies
-        ));
+        // findAll() - SELECT * FROM movies;
+        // find(5) - SELECT * FROM movies WHERE id=5;
+        // findBy([],['id' => 'DESC']) - SELECT * FROM movies order by id desc;
+        // findOneBy(['id' => 8, 'title'=>'The Dark 2'],['id' => 'DESC']);
+        // count(['id' => 8]);
+       $repository = $this->em->getRepository(Movie::class);
+       $movies = $repository->getClassName();
+
+       dd($movies);
+
+        return $this->render('index.html.twig' );
         
     }    
+       
 
 }
